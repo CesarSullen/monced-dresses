728a08edb521933470a3c992482a631f3a879c7f
Cesar <cesarsuarez2303@gmail.com>
2025-07-05 15:53:21 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// Nav Bar Toggle
+let toggleBtn = document.getElementById("toggle-btn");
+let body = document.getElementById("body");
+let links = document.querySelectorAll(".side-bar-link");
+let sideBar = document.getElementById("side-bar");
+
+toggleBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	toggleBtn.classList.toggle("toggled");
+
+	if (
+		sideBar.style.transform === "translateX(-200%)" ||
+		sideBar.style.transform === ""
+	) {
+		sideBar.style.transform = "translateX(0px)";
+		body.style.overflowY = "hidden";
+	} else {
+		sideBar.style.transform = "translateX(-200%)";
+		body.style.overflowY = "auto";
+	}
+}
+
+links.forEach((link) =>
+	link.addEventListener("click", () => {
+		sideBar.style.transform = "translateX(200%)";
+		body.style.overflowY = "auto";
+		toggleBtn.classList.toggle("toggled");
+	})
+);
+
+// Interception Observer
+const sections = document.querySelectorAll(
+	".fade-in, .rotate-in, .zoom-up, .blur-out"
+);
+
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			entry.target.classList.toggle("animated", entry.isIntersecting);
+			if (entry.isIntersecting) {
+				observer.unobserve(entry.target);
+			}
+		});
+	},
+	{
+		threshold: 0.1,
+	}
+);
+
+sections.forEach((section) => {
+	observer.observe(section);
+});
